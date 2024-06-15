e53da05aa9b7c506e20e7973bdae06181bca11f8
JesiLyss <antonella.jesica.gomez@gmail.com>
2024-06-15 00:47:47 -0300

    mods

--- main.js
+++ main.js
@@ -4,6 +4,20 @@ function playsonidopom() {
 
 }
 
+document.querySelectorAll('.tecla')
 
-document.querySelector('.tecla_pom');
 
+
+
+//document.querySelector('.tecla_pom').onclick = playsonidopom;
+
+
+
+//function playsonidoclap() {
+
+    /*document.querySelector('#sonido_tecla_clap').play();
+
+}
+
+
+//document.querySelector('.tecla_clap').onclick = playsonidoclap;
